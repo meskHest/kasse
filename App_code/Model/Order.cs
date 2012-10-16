35b50508e12f57731f4cf8be152d2ac07d0777d1
Fredrik Olsson <fredrik@mrolsson.se>
2012-10-16 14:34:42 +0200

    added Order model

---
+++ App_code/Model/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Order
+/// </summary>
+public class Order
+{
+    public int id { get; set; }
+    public int MyProperty { get; set; }
+
+	public Order()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
