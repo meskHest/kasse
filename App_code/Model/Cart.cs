070df453713a1f286195684fd1cc648776d6b784
Fredrik Olsson <fredrik@mrolsson.se>
2012-10-16 14:10:43 +0200

    Added Cart class

---
+++ App_code/Model/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Cart
+/// </summary>
+public class Cart
+{
+
+
+	public Cart()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
