2ed56831c42c97e2632e3a9d762efcf4ddbd01e3
Daniel Kannerstedt <kannerstedt@gmail.com>
2012-10-16 14:58:07 +0200

    added class

---
+++ App_code/Model/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Department
+/// </summary>
+public class Department
+{
+    public int id { get; set; }
+    public string name { get; set; }
+
+	public Department()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
