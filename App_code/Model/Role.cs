12722c08b6219ebc309b0e77a06c3767af70527d
Christoffer Andersson <Toffe19@hotmail.com>
2012-10-16 14:54:35 +0200

    added role class

---
+++ App_code/Model/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Role
+/// </summary>
+public class Role
+{
+    public int id { get; set; }
+    public string roleName { get; set; }
+    
+    public Role()
+	{
+		
+	}
+}
