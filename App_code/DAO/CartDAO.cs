70b02e08eb651fdc46e010cdc8587b678227ebae
Fredrik Olsson <fredrik@mrolsson.se>
2012-10-16 14:11:54 +0200

    Added CartDAO

---
+++ App_code/DAO/CartDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CartDAO
+/// </summary>
+public class CartDAO
+{
+	public CartDAO()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
