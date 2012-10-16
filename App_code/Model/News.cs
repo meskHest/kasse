e585ee74397cb1652c38331e6da812f3363ec6b3
Daniel Kannerstedt <kannerstedt@gmail.com>
2012-10-16 15:03:50 +0200

    added class

---
+++ App_code/Model/News.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for News
+/// </summary>
+public class News
+{
+    public int id { get; set; }
+    public int storeId { get; set; }
+    public int userId { get; set; }
+    public DateTime created { get; set; }
+    public string headline { get; set; }
+    public string message { get; set; }
+
+	public News()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
