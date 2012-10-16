4cd43a40f711fa1a5653255f9872ebfc85e55bf0
Christoffer Andersson <Toffe19@hotmail.com>
2012-10-16 14:13:28 +0200

    Added UserDAO

---
+++ App_code/DAO/UserDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserDAO
+/// </summary>
+public class UserDAO
+{
+	public UserDAO()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
