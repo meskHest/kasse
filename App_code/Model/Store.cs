33306d540152fe828e8555139771c1a799250dc7
Christoffer Andersson <Toffe19@hotmail.com>
2012-10-16 15:02:05 +0200

    added store class

---
+++ App_code/Model/Store.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Store
+/// </summary>
+public class Store
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string adress { get; set; }
+    public string info { get; set; }
+    public bool active { get; set; }
+    
+    public Store()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
