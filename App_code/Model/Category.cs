57b490e9a952d681543c79f3328a88909b72d11e
Christoffer Andersson <Toffe19@hotmail.com>
2012-10-16 14:59:15 +0200

    added category class

---
+++ App_code/Model/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Category
+/// </summary>
+public class Category
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    
+    public Category()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
